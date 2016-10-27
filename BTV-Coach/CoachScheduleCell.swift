5b3d0d7cddd7b0c18685a225b247306e97963650
Deepinder-Lakhi <logicielsolution@LogicielSolutions-Mac-mini.local>
2016-10-27 14:25:06 +0530

    Coach DashBoard VC

---
+++ BTV-Coach/CoachScheduleCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoachScheduleCell.swift
+//  BTV-Coach
+//
+//  Created by LogicielSolution on 27/10/16.
+//  Copyright © 2016 AppRoots.tech. All rights reserved.
+//
+
+import UIKit
+
+class CoachScheduleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
