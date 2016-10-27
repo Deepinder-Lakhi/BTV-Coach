12c6147beeee4aa94ad607f683b339d63b12383d
Deepinder-Lakhi <prabhat@Prabhats-MacBook-Air.local>
2016-10-27 14:28:46 +0530

    change availability

---
+++ BTV-Coach/BTVChangeAvailability.swift
@@ -0,0 +1,35 @@
+//
+//  BTVChangeAvailability.swift
+//  BTV-Coach
+//
+//  Created by Prabhat on 27/10/16.
+//  Copyright © 2016 AppRoots.tech. All rights reserved.
+//
+
+import UIKit
+
+class BTVChangeAvailability: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BTV-Coach/BTVChangeAvailabilityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BTVChangeAvailabilityTableViewCell.swift
+//  BTV-Coach
+//
+//  Created by Prabhat on 27/10/16.
+//  Copyright © 2016 AppRoots.tech. All rights reserved.
+//
+
+import UIKit
+
+class BTVChangeAvailabilityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
